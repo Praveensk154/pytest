bcf9ced1d6bd5b41ec6da5d78a25837429eca0f6
Praveensk154 <Skpraveen154@gmail.com>
2023-07-06 17:02:21 +0530

    third commit

--- PageObjects/LoginPage.py
+++ PageObjects/LoginPage.py
@@ -15,31 +15,3 @@ class Test_login:
     def clicksignout(self):
         self.driver.find_element_by_xpath(self.Sign_out_xpath).click()
 
-class Homepage:
-    All_category_xpath="//*[@id='gh-cat']"
-    Cell_phones_value="15032"
-    Search_option_class="btn btn-prim gh-spr"
-    Cell_smartphone_class="b-visualnav__title"
-    filter_button_type="button"
-    SwitchFrame_class="x-overlay__container"
-
-
-    def __int__(self,driver):
-        self.driver=driver
-
-    def AllCategory(self):
-        self.driver.find_element_by_xpath(self.All_category_xpath).clear()
-        self.driver.find_element_by_xpath(self.All_category_xpath).click()
-
-    def Search_option(self):
-        self.driver.find_element_by_class(self.Search_option_class).click()
-
-    def Cell_phones(self):
-        self.driver.find_element_by_class(self.Cell_smartphone_class).click()
-
-    def Filter_button(self):
-        self.driver.find_element_by_type(self.filter_button_type).click()
-
-    def Switch_frame(self):
-        self.driver.Switch_to.frame()
-
